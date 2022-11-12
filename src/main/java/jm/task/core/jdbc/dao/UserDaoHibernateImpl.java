c123777305c219db6f77b1892db0e769e7d6da4e
vladimirKutyavin <kva.devops@gmail.com>
2022-11-12 14:58:39 +0400

    Java pre-project. Практическая задача 1.1.4 - ИСПРАВЛЕНИЯ:
    - UserDaoHibernateImpl.java исправлена реализация всех методов с использованием паттерна проектирования "wrapper"

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,12 +4,29 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class UserDaoHibernateImpl implements UserDao {
     private final SessionFactory sessionFactory = Util.getSessionFactory();
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sessionFactory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
 
     public UserDaoHibernateImpl() {
 
@@ -17,62 +34,41 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-            var sql = """
-                CREATE TABLE IF NOT EXISTS users (
-                    id SERIAL PRIMARY KEY,
-                    name VARCHAR(255),
-                    lastname VARCHAR(255),
-                    age SMALLINT
-                );
-                """;
-            var query = session.createSQLQuery(sql).addEntity(User.class);
-            query.executeUpdate();
-            session.getTransaction().commit();
-        }
+        this.tx(session -> session.createSQLQuery("""
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255),
+                lastname VARCHAR(255),
+                age SMALLINT
+            );
+        """).addEntity(User.class).executeUpdate());
     }
 
     @Override
     public void dropUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-            var sql = "DROP TABLE IF EXISTS users";
-            var query = session.createSQLQuery(sql).addEntity(User.class);
-            query.executeUpdate();
-            session.getTransaction().commit();
-        }
+        this.tx(session -> session.createSQLQuery("""
+            DROP TABLE IF EXISTS users
+        """).addEntity(User.class).executeUpdate());
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        User user = new User(name, lastName, age);
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-        }
+        this.tx(session -> session.save(new User(name, lastName, age)));
         System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
     @Override
     public void removeUserById(long id) {
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+        this.tx(session -> {
             User user = session.find(User.class, id);
             session.delete(user);
-            session.getTransaction().commit();
-        }
+            return null;
+        });
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> result;
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-            result = session.createQuery("from User").list();
-            session.getTransaction().commit();
-        }
+        List<User> result = this.tx(session -> session.createQuery("from User").list());
         result.forEach(user -> System.out.println(user.toString()));
         return result;
     }
@@ -80,13 +76,12 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+        this.tx(session -> {
             List<User> users = session.createQuery("from User").list();
             for (User user : users) {
                 session.delete(user);
             }
-            session.getTransaction().commit();
-        }
+            return null;
+        });
     }
 }
